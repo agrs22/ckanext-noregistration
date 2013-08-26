3340cb064c41ff9fbdb4833ed4356ace725025fa
Stefan Reinhard <sr@feinheit.ch>
2013-08-26 16:25:51 +0200

    remove registration link in header when active

--- ckanext/noregistration/plugins.py
+++ ckanext/noregistration/plugins.py
@@ -1,13 +1,17 @@
-from ckan import plugins
+from ckan import plugins as p
 
 def no_registering(context, data_dict):
-    return {'success': False, 'msg': plugins.toolkit._('You cannot register '
+    return {'success': False, 'msg': p.toolkit._('You cannot register '
         'for this site.')}
 
-class NoSelfRegistration(plugins.SingletonPlugin):
-    plugins.implements(plugins.IAuthFunctions, inherit=True)
+class NoSelfRegistration(p.SingletonPlugin):
+    p.implements(p.IAuthFunctions, inherit=True)
+    p.implements(p.IConfigurer)
 
     def get_auth_functions(self):
         return {
             'user_create': no_registering
         }
+
+    def update_config(self, config):
+        p.toolkit.add_template_directory(config, 'templates')
