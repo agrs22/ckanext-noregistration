2ade0ce966d09fce3b783da011ab64c6f57967bb
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-12-20 00:02:54 +0100

    Add PEP8 code style check

--- ckanext/noregistration/plugins.py
+++ ckanext/noregistration/plugins.py
@@ -1,8 +1,14 @@
 from ckan import plugins as p
 
+
 def no_registering(context, data_dict):
-    return {'success': False, 'msg': p.toolkit._('You cannot register '
-        'for this site.')}
+    return {
+        'success': False,
+        'msg': p.toolkit._(
+            'You cannot register for this site.'
+        )
+    }
+
 
 class NoSelfRegistration(p.SingletonPlugin):
     p.implements(p.IAuthFunctions, inherit=True)
